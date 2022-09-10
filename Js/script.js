ddae742f5433e128798f1c4dc397cd7259062f4a
alexweb185 <alexnavia.dark@gmail.com>
2022-09-10 15:19:56 -0400

    js

---
+++ Js/script.js
@@ -0,0 +1,17 @@
+
+//entrada nombre
+let nombreIngresado = prompt("Ingrese su nombre");
+
+let salida = "tu nombre es: " + nombreIngresado;
+
+alert(salida);
+
+//entrada numero
+let numero2 = 20;
+
+let numeroIngresado = prompt("Ingrese un numero");
+
+let operacion = numero2 * numeroIngresado;
+let salidaNumero = "20 * " + numeroIngresado + " es igual a: " + operacion;
+
+alert(salidaNumero);
